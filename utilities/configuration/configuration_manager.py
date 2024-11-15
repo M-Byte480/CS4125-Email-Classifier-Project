3ad505d7aaee021d42dab72f9fdb3a7a207d9cfd
Milan <kmilanm13@gmail.com>
2024-11-15 12:32:21 +0000

    [Milan] Me n Bayan cooked

---
+++ utilities/configuration/configuration_manager.py
@@ -0,0 +1,16 @@
+from Config import Config
+
+# Singleton instance
+class ConfigurationManager:
+    instance = None
+
+    def __new__(cls):
+        if cls.instance is None: # If instance is not created, create it
+            cls.instance = super(ConfigurationManager, cls).__new__(cls)
+            cls.instance.config = Config()
+            return cls.instance
+        return cls.instance
+
+    def get_config(self):
+        return self.config
+
