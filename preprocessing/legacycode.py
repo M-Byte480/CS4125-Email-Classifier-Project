07b0f41ef9ebf0e36bbfad95789a0e8cde38d02c
Jacob' 'Beck <102110500+ShaveYourEyebrows@users.noreply.github.com>
2024-11-22 21:16:15 +0000

    Shoehorned Adapter Pattern
    Changed code to utilise an adapter pattern. We have to mention that we're changing our spec! The customer has legacy translation code that they want compatible with a list-based interface, but without editing their old code.

---
+++ preprocessing/legacycode.py
@@ -0,0 +1,81 @@
+import warnings
+import numpy as np
+import stanza
+from stanza.pipeline.core import DownloadMethod
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
+
+from utilities.logger.error_logger import ErrorLogger
+from utilities.logger.indentation_decorator import IndentationDecorator
+from utilities.logger.warning_logger import WarningLogger
+
+#The legacy translator code, which we need to adapt wo work with a list-based interface
+class OldTranslator:
+    @staticmethod
+    def trans_to_en(texts : np.array):
+        error_logger = IndentationDecorator(ErrorLogger("Translator"), "[trans_to_en]")
+        warning_logger = IndentationDecorator(WarningLogger("Translator"), "[trans_to_en]")
+        with warnings.catch_warnings(record=True) as caught_warning:
+
+            t2t_m = "facebook/m2m100_418M"
+
+            model = M2M100ForConditionalGeneration.from_pretrained(t2t_m)
+            tokenizer = M2M100Tokenizer.from_pretrained(t2t_m)
+            nlp_stanza = stanza.Pipeline(lang="multilingual",
+                                         processors="langid",
+                                         download_method=DownloadMethod.REUSE_RESOURCES)
+            language_map = {
+                "fro": "fr",  # Old French
+                "la": "it",  # Latin
+                "nn": "no",  # Norwegian (Nynorsk)
+                "kmr": "tr",  # Kurmanji
+                "mt": "pl"   # maltese to polish because there is no maltese (in the dataset or the model)
+            }
+
+            text_en_l = []
+            for text in texts:
+                # Empty strings get appended
+                if text == "":
+                    text_en_l.append("")
+                    continue
+                try:
+                    doc = nlp_stanza(text)
+                    detected_lang = doc.lang
+
+                    # If language is english append
+                    if detected_lang == "en":
+                        text_en_l.append(text)
+                        continue
+                    # Detected lang
+                    detected_lang = language_map.get(detected_lang, detected_lang)
+
+                    tokenizer.src_lang = detected_lang
+                    encoded_hi = tokenizer(text, return_tensors="pt")
+                    generated_tokens = model.generate(
+                        **encoded_hi,
+                        forced_bos_token_id=tokenizer.get_lang_id("en")
+                    )
+                    text_en = tokenizer.decode(generated_tokens[0], skip_special_tokens=True)
+
+                    text_en_l.append(text_en)
+
+
+                except Exception as e:
+                    error_logger.log("Error occured")
+                    error_logger.log(str(e))
+
+            for warning in caught_warning:
+                if issubclass(warning.category, FutureWarning):
+                    warning_logger.log(f"{warning.message}")
+
+        return text_en_l
+
+#Adapter to use legacy code on new interface
+class TranslatorAdaptor(OldTranslator):
+
+    texts : np.array
+
+    def __init__(self, texts : list):
+        self.texts = np.array(texts)
+
+    def trans_to_en(self):
+        return super().trans_to_en(self.texts)
--- preprocessing/processor.py
+++ preprocessing/processor.py
@@ -1,17 +1,12 @@
-import warnings
-
 import numpy as np
 import pandas as pd
-import stanza
 
-from stanza.pipeline.core import DownloadMethod
-from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from utilities.configuration.config import Config
 from utilities.logger.error_logger import ErrorLogger
-from utilities.logger.indentation_decorator import IndentationDecorator
 from utilities.logger.info_logger import InfoLogger
 from utilities.logger.warning_logger import WarningLogger
+from legacycode import TranslatorAdaptor
 
 
 # Singleton instance
@@ -181,60 +176,6 @@ class DataProcessor:
 
     # Translation
     @staticmethod
-    def trans_to_en(texts):
-        error_logger = IndentationDecorator(DataProcessor.error_logger, "[trans_to_en]")
-        warning_logger = IndentationDecorator(DataProcessor.warning_logger, "[trans_to_en]")
-        with warnings.catch_warnings(record=True) as caught_warning:
-
-            t2t_m = "facebook/m2m100_418M"
-
-            model = M2M100ForConditionalGeneration.from_pretrained(t2t_m)
-            tokenizer = M2M100Tokenizer.from_pretrained(t2t_m)
-            nlp_stanza = stanza.Pipeline(lang="multilingual",
-                                         processors="langid",
-                                         download_method=DownloadMethod.REUSE_RESOURCES)
-            language_map = {
-                "fro": "fr",  # Old French
-                "la": "it",  # Latin
-                "nn": "no",  # Norwegian (Nynorsk)
-                "kmr": "tr",  # Kurmanji
-                "mt": "pl"   # maltese to polish because there is no maltese (in the dataset or the model)
-            }
-
-            text_en_l = []
-            for text in texts:
-                # Empty strings get appended
-                if text == "":
-                    text_en_l.append("")
-                    continue
-                try:
-                    doc = nlp_stanza(text)
-                    detected_lang = doc.lang
-
-                    # If language is english append
-                    if detected_lang == "en":
-                        text_en_l.append(text)
-                        continue
-                    # Detected lang
-                    detected_lang = language_map.get(detected_lang, detected_lang)
-
-                    tokenizer.src_lang = detected_lang
-                    encoded_hi = tokenizer(text, return_tensors="pt")
-                    generated_tokens = model.generate(
-                        **encoded_hi,
-                        forced_bos_token_id=tokenizer.get_lang_id("en")
-                    )
-                    text_en = tokenizer.decode(generated_tokens[0], skip_special_tokens=True)
-
-                    text_en_l.append(text_en)
-
-
-                except Exception as e:
-                    error_logger.log("Error occured")
-                    error_logger.log(str(e))
-
-            for warning in caught_warning:
-                if issubclass(warning.category, FutureWarning):
-                    warning_logger.log(f"{warning.message}")
-
-        return text_en_l
+    def trans_to_en(texts : list):
+        ta = TranslatorAdaptor(texts)
+        return ta.trans_to_en()
